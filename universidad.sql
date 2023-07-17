682d5931594ef796388f7fc436f9a5b3aba910d1
lucho9595 <lucianocoronel48@gmail.com>
2023-07-16 23:32:59 -0300

    hasta aca llegue

--- universidad.sql
+++ universidad.sql
@@ -16,34 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `carreramateria`
---
-
-DROP TABLE IF EXISTS `carreramateria`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `carreramateria` (
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  `carrera_id` int NOT NULL,
-  `materia_id` int NOT NULL,
-  PRIMARY KEY (`carrera_id`,`materia_id`),
-  KEY `materia_id` (`materia_id`),
-  CONSTRAINT `carreramateria_ibfk_1` FOREIGN KEY (`carrera_id`) REFERENCES `carreras` (`id_carrera`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `carreramateria_ibfk_2` FOREIGN KEY (`materia_id`) REFERENCES `materia` (`id_materia`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `carreramateria`
---
-
-LOCK TABLES `carreramateria` WRITE;
-/*!40000 ALTER TABLE `carreramateria` DISABLE KEYS */;
-/*!40000 ALTER TABLE `carreramateria` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `carreras`
 --
 
@@ -58,7 +30,7 @@ CREATE TABLE `carreras` (
   `facultad` varchar(150) NOT NULL,
   `año_cursada` int NOT NULL,
   PRIMARY KEY (`id_carrera`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +39,7 @@ CREATE TABLE `carreras` (
 
 LOCK TABLES `carreras` WRITE;
 /*!40000 ALTER TABLE `carreras` DISABLE KEYS */;
-INSERT INTO `carreras` VALUES (1,'Contador Publico','abocado a la contabilidad publica','1920-10-02 04:00:00','Economia',6),(2,'Administracion de empresas','abocado a la administracion','1930-10-02 04:00:00','Economia',6),(3,'Ingeniero informatico','abocado a la informatica','1900-10-02 04:16:48','Informatica',6),(4,'Medicina','abocado a la medicina','1850-10-02 03:53:48','Medicina',6);
+INSERT INTO `carreras` VALUES (1,'Contador Publico','abocado a la contabilidad publica','1920-10-02 04:00:00','Economia',6),(2,'Administracion de empresas','abocado a la administracion','1930-10-02 04:00:00','Economia',6),(3,'Ingeniero informatico','abocado a la informatica','1900-10-02 04:16:48','Informatica',6),(4,'Medicina','abocado a la medicina','1850-10-02 03:53:48','Medicina',6),(5,'Ingeniero Quimico','es un ingeniero quimico','2023-07-06 00:00:00','Ingenieria',6);
 /*!40000 ALTER TABLE `carreras` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,13 +52,13 @@ DROP TABLE IF EXISTS `inscripcions`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `inscripcions` (
   `id_inscripcion` int NOT NULL AUTO_INCREMENT,
-  `materia_id` int NOT NULL,
   `usuario_id` int DEFAULT NULL,
+  `materia_id` int DEFAULT NULL,
   PRIMARY KEY (`id_inscripcion`),
-  UNIQUE KEY `Inscripcions_materia_id_usuario_id_unique` (`materia_id`,`usuario_id`),
-  KEY `usuario_id` (`usuario_id`),
-  CONSTRAINT `inscripcions_ibfk_2` FOREIGN KEY (`materia_id`) REFERENCES `materia` (`id_materia`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `inscripcions_ibfk_3` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `materia_id` (`materia_id`),
+  KEY `inscripcion_ibfk_1_idx` (`usuario_id`),
+  CONSTRAINT `inscripcions_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id_usuario`),
+  CONSTRAINT `inscripcions_ibfk_2` FOREIGN KEY (`materia_id`) REFERENCES `materia` (`id_materia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -114,9 +86,9 @@ CREATE TABLE `materia` (
   `carrera_id` int DEFAULT NULL,
   `año_cursada` int DEFAULT NULL,
   PRIMARY KEY (`id_materia`),
-  KEY `MATERIA` (`carrera_id`),
+  KEY `carrera_id` (`carrera_id`),
   CONSTRAINT `MATERIA` FOREIGN KEY (`carrera_id`) REFERENCES `carreras` (`id_carrera`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +97,7 @@ CREATE TABLE `materia` (
 
 LOCK TABLES `materia` WRITE;
 /*!40000 ALTER TABLE `materia` DISABLE KEYS */;
-INSERT INTO `materia` VALUES (1,'Matematica 1',60,'Promoción',1,1),(2,'Algebra 1',60,'Promoción',1,1),(3,'Anatomia',60,'Promoción',4,1),(4,'Fisiologia',60,'Promoción',4,2),(5,'Quimica',60,'Promoción',4,1),(6,'Embriologia',60,'Promoción',4,1),(7,'Bioetica',60,'Promoción',4,2),(8,'Patologia',60,'Promoción',4,1),(9,'Logistica',60,'Promoción',3,2),(10,'Principios de Programacion',60,'Examen Final',3,1),(11,'Principios de Logica',60,'Examen Final',3,1),(12,'Matematica 2',60,'Examen Final',1,2);
+INSERT INTO `materia` VALUES (1,'Matematica 1',60,'Promoción',1,1),(2,'Algebra 1',60,'Promoción',1,1),(3,'Anatomia',60,'Promoción',4,1),(4,'Fisiologia',60,'Promoción',4,2),(5,'Quimica',60,'Promoción',4,1),(6,'Embriologia',60,'Promoción',4,1),(7,'Bioetica',60,'Promoción',4,2),(8,'Patologia',60,'Promoción',4,1),(9,'Logistica',60,'Promoción',3,2),(10,'Principios de Programacion',60,'Examen Final',3,1),(11,'Principios de Logica',60,'Examen Final',3,1),(12,'Matematica 2',60,'Examen Final',1,2),(14,'Historia 5',20,'Promoción',1,5),(15,'Matematica 5',20,'Promoción',1,5);
 /*!40000 ALTER TABLE `materia` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,7 +130,7 @@ CREATE TABLE `nota` (
   CONSTRAINT `nota_chk_2` CHECK (((`nota2` >= 1) and (`nota2` <= 10))),
   CONSTRAINT `nota_chk_3` CHECK (((`nota3` >= 1) and (`nota3` <= 10))),
   CONSTRAINT `nota_chk_4` CHECK (((`nota4` >= 1) and (`nota4` <= 10)))
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,6 +139,7 @@ CREATE TABLE `nota` (
 
 LOCK TABLES `nota` WRITE;
 /*!40000 ALTER TABLE `nota` DISABLE KEYS */;
+INSERT INTO `nota` VALUES (8,3.00,6.00,9.00,9.00,7,19,5),(9,3.00,6.00,9.00,9.00,7,19,3);
 /*!40000 ALTER TABLE `nota` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -188,13 +161,13 @@ CREATE TABLE `usuarios` (
   `domicilio` varchar(150) NOT NULL,
   `carrera_id` int DEFAULT NULL,
   `rol` varchar(20) DEFAULT 'estudiante',
-  `password` varchar(15) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `año_cursada` int NOT NULL DEFAULT '1',
-  `materias_asignadas` text,
+  `materias_asignadas` json DEFAULT NULL,
   PRIMARY KEY (`id_usuario`),
   KEY `usuarios_ibfk_1` (`carrera_id`),
   CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`carrera_id`) REFERENCES `carreras` (`id_carrera`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -203,7 +176,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'admin',9999,9999,'admin@admin.com',99,9999,'XXXX 9999',NULL,'admin','admin123456',0,'0'),(2,'Melany Avalos',40516452,1152415674,'melany@melany.com',25,1881,'846 5526',4,'estudiante','123456',1,'0'),(3,'Gary Coronel Avalos',60516452,1135815674,'gary@gary.com',25,1881,'846 5526',2,'estudiante','123456',1,'0'),(4,'Alberto Gomez',17516452,1196575674,'alberto@alberto.com',40,1471,'846 5526',1,'estudiante','123456',2,'0'),(5,'Somos Guan',23566452,1199687674,'somos@somos.com',23,1471,'846 5526',1,'estudiante','123456',2,'0'),(6,'Luciano Coronel',23516452,1152468974,'luciano@luciano.com',28,1828,'Rawson 5526',3,'estudiante','123456',1,'0'),(11,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(12,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(13,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(14,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(15,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(16,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(17,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(18,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(19,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(20,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(21,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'0'),(22,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(23,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(24,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(25,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(26,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(27,'Nombre Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(28,'Nombre y Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(29,'Nombre y Apellido',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(30,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(31,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'\"[]\"'),(32,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(33,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(34,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(35,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(36,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(37,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(38,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(39,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(40,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(41,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[]'),(42,'Nombre',12345678,123456789,'correo@example.com',25,1234,'Calle 123',1,'estudiante','contraseña',1,'[1,2]');
+INSERT INTO `usuarios` VALUES (18,'admins',12345678,123456789,'admin@admin.com',25,1234,'Calle admin',1,'admin','$2b$10$NgeL2HSC0u/jP17OQaYMNOst8qVZWFPc9dcRC5cC3tqBoKE.G0xQC',0,'0'),(19,'Luciano Coronel',38666654,1137601819,'luciano@luciano.com',30,1828,'Calle 1',4,'estudiante','$2b$10$bZidVMYSGkYt2WQoOoDEtOxEC4ndxFeaqpfUIuwo6hnwfM2rDrwz.',1,'[3, 5, 6, 8]'),(20,'ve larga',38636805,1137601819,'a@a.com',29,1234,'Rawson 1152',4,'estudiante','$2b$10$5.WTSgF0XlmRN0EfrR.PS.g5yGMCH9CqzyJyVBbS22i7k3uWU6xFS',1,'[3, 5, 6, 8]');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -216,4 +189,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-14 16:54:21
+-- Dump completed on 2023-07-16 23:32:35
