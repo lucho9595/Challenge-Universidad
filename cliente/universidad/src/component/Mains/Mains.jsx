ea3ee1bccedee38a4a1c4c13b80329f7ab156905
lucho9595 <lucianocoronel48@gmail.com>
2023-07-16 23:38:35 -0300

    fin.

--- cliente/universidad/src/component/Mains/Mains.jsx
+++ cliente/universidad/src/component/Mains/Mains.jsx
@@ -8,13 +8,13 @@ export default function Mains() {
                 <div className="container px-4">
                     <div className="row gx-4 justify-content-center">
                         <div className="col-lg-8">
-                            <h2>About this page</h2>
-                            <p className="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
+                            <h2>SOBRE ESTA PAGINA</h2>
+                            <p className="lead">Esto es un proyecto para entregar</p>
                             <ul>
-                                <li>Clickable nav links that smooth scroll to page sections</li>
-                                <li>Responsive behavior when clicking nav links perfect for a one page website</li>
-                                <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
-                                <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+                                <li>Esta estructurado en 2 perfiles</li>
+                                <li>hay un usuario admin y otro para el estudiante</li>
+                                <li>ambos tiene opciones distintas a realizar y para visualizar</li>
+                                <li>espero que les guste.</li>
                             </ul>
                         </div>
                     </div>
